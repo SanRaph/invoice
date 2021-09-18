20e1093a41121e3412702e86a6de49148086d8b5
everybees <everybees@gmail.com>
2021-09-18 22:48:22 +0100

    added timer

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import upload_process
+import os, time
 
 
-def main():
-    config_data = upload_process.read_config_data()
-    files = upload_process.get_files_in_folder((config_data.get("source_folder") + "*"))
+config_data = upload_process.read_config_data()
 
+
+def main(files):
     # process files
     upload_process.process_files(files)
 
 
 if __name__ == "__main__":
-    main()
+    path_to_watch = config_data.get("source_folder")
+    while True:
+        time.sleep(60)
+        files = upload_process.get_files_in_folder((config_data.get("source_folder") + "*"))
+        if files:
+            main(files)
