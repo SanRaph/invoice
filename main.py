663e6987c5aa8b7d5eddd9fa34966581376481fa
everybees <everybees@gmail.com>
2021-09-23 19:03:31 +0100

    outstanding

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
-import upload_process
 
-config_data = upload_process.read_config_data()
+import functions as fun
+from upload_process import process_files
+
+config_data = fun.read_config_data()
 
 
 def main(files_):
     # process files
-    upload_process.process_files(files_)
+    process_files(files_)
 
 
 if __name__ == "__main__":
@@ -14,9 +16,9 @@ if __name__ == "__main__":
     while True:
         print("Checking files in Source Folder")
         time.sleep(config_data.get('refresh_time'))
-        files = upload_process.get_files_in_folder((config_data.get("source_folder") + "*"))
+        files = fun.get_files_in_folder((config_data.get("source_folder") + "*"))
         if files:
             print(f"{len(files)} found. Starting operation.")
             main(files)
         else:
-            print(f"No files found. Will start operation in another {config_data.get('refresh_time')}")
+            print(f"No files found. Will start operation in another {config_data.get('refresh_time')} seconds")
--- upload_process.py
+++ upload_process.py
@@ -1,83 +1,22 @@
-import base64
-import glob
 import json
-import shutil
-import datetime
 
 import cv2
-import pdfplumber
 import requests
 from pdf2image import convert_from_path
 from pyzbar import pyzbar
 
-
-def read_config_data():
-    with open('config.txt', 'r') as config_file:
-        config_data = json.load(config_file)
-    return config_data
-
-
-def get_files_in_folder(folder):
-    return glob.glob(folder)
-
-
-def convert_pdf_to_text(file_path):
-    pdf = pdfplumber.open(file_path)
-    pages = pdf.pages
-    full_text = ""
-    for page in pages:
-        text = page.extract_text()
-        full_text += text
-    pdf.close()
-    return full_text
-
-
-def write_log(barcode, response):
-    now = datetime.datetime.now()
-    file = open(f'logs/{now.strftime("%b-%d-%Y")}.log', 'a+')
-    log = str(now) + " " + barcode + " : " + response
-    file.write(f"{log}\n")
-
-
-def move_file_to_folder(file_name):
-    config_data = read_config_data()
-    current_folder = config_data.get('source_folder')
-    new_folder = config_data.get('destination_folder')
-    shutil.move(current_folder + file_name, new_folder + file_name)
-
-
-def get_file_content(file_path):
-    with open(file_path, 'rb') as file:
-        encode = base64.b64encode(file.read())
-    return encode
-
-
-def get_image_content(file_path):
-    with open(file_path, 'rb') as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-    return encoded_string
+import functions as fun
+from get_headers import get_headers
 
 
 def upload_barcode(file_path):
-    config_data = read_config_data()
+    config_data = fun.read_config_data()
     url = config_data.get('api_link')
-    consumer_key = config_data.get('consumer_key')
-    consumer_key = f'"{consumer_key}"'
-    access_token = config_data.get("access_token")
-    access_token = f'"{access_token}"'
     file_split = file_path.split('/')[-1].split('.')
     file_name, file_extension = file_split[0], file_split[1]
     file = file_name + '.' + file_extension
 
-    headers = {
-        'Authorization': f'OAuth realm="5774630",'
-                         f'oauth_consumer_key={consumer_key},'
-                         f'oauth_token={access_token},'
-                         f'oauth_signature_method="HMAC-SHA1",oauth_timestamp="1631997167",oauth_nonce="sVPRo7oyi0K",'
-                         f'oauth_version="1.0",oauth_signature="TXyyrN1APC7GpkhZENpg%2Fvuk72M%3D"',
-        'Content-Type': 'application/json',
-        'Cookie': 'NS_ROUTING_VERSION=LAGGING'
-    }
+    print("Trying for file ", file)
 
     if file_extension.lower() == "pdf":
         pdf_page = convert_from_path(file_path)
@@ -95,6 +34,7 @@ def upload_barcode(file_path):
 
         # convert into string
         b_data = barcode.data.decode("utf-8")
+        payload = {}
 
         if file_extension.lower() == "pdf":
             payload = {
@@ -102,21 +42,31 @@ def upload_barcode(file_path):
                 "recordNumber": b_data,
                 "fileType": file_extension.upper(),
                 "fileName": file,
-                "fileContent": get_file_content(file_path).decode('utf-8')
+                "fileContent": fun.get_file_content(file_path).decode('utf-8')
             }
-        else:
+        elif file_extension.lower() == "png":
             payload = {
                 "recordType": "invoice",
                 "recordNumber": b_data,
-                "fileType": file_extension.upper(),
+                "fileType": "PNGIMAGE",
                 "fileName": file,
-                "fileContent": get_image_content(file_path).decode('utf-8')
+                "fileContent": fun.get_image_content(file_path).decode('utf-8')
+            }
+        elif file_extension.lower() == "jpg":
+            payload = {
+                "recordType": "invoice",
+                "recordNumber": b_data,
+                "fileType": "JPGIMAGE",
+                "fileName": file,
+                "fileContent": fun.get_image_content(file_path).decode('utf-8')
             }
 
-        response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+        headers = get_headers()
+
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
         if response.status_code == 200 and "success" in response.text:
-            move_file_to_folder(file)
-        write_log(payload['recordNumber'], response.text)
+            fun.move_file_to_folder(file)
+        fun.write_log(payload['recordNumber'], file, response.text)
 
 
 def process_files(files):
